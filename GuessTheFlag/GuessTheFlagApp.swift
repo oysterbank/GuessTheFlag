5c93432a5209c02b12fcfa33396289013c7e1d25
Kris Laratta <krislaratta@gmail.com>
2022-10-02 16:59:22 -0400

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kris Laratta on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
