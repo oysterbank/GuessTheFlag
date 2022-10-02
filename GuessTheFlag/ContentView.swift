dbba3d45160eaed2a05a2e86d01ae2df193c24e8
Kris Laratta <krislaratta@gmail.com>
2022-10-02 19:24:52 -0400

    inform user of correct flag on wrong guess

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -76,7 +76,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
         } else {
             userScore -= 10
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
         
         showingScore = true
